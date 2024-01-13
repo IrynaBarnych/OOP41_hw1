c90f42499ed6969faafada06b3b3943a620e6301
IrynaBarnych <iryna.barnych@gmail.com>
2024-01-13 02:17:58 +0200

    Завдання 4 Створіть однотабличну базу даних «Овочі та фрукти», яка зберігатиме таку інформацію

--- hw1.sql
+++ hw1.sql
@@ -1,4 +1,19 @@
---Завдання 3
---Видаліть базу даних Cats
+--Завдання 4
+--Створіть однотабличну базу даних «Овочі та фрукти»,
+--яка зберігатиме таку інформацію:
 
-DROP DATABASE IF EXISTS Cats;
+CREATE TABLE VegetablesAndFruits (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    type VARCHAR(10), -- "овоч" або "фрукт"
+    color VARCHAR(50),
+    calories INT,
+    description TEXT
+);
+
+-- Вставка даних
+INSERT INTO VegetablesAndFruits (name, type, color, calories, description)
+VALUES
+    ('Помідор', 'овоч', 'червоний', 18, 'Смачний та корисний овоч, багатий вітамінами.'),
+    ('Яблуко', 'фрукт', 'червоний', 52, 'Солодке та соковите яблуко.'),
+    ('Брокколі', 'овоч', 'зелений', 55, 'Здоровий овоч, багатий вітамінами та мінералами.');
